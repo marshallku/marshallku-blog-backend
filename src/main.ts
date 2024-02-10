482a4e88616fb00b32514c8bc5f5c384cf085f36
Marshall Ku <marshall@kakao.com>
2024-02-11 01:35:29 +0900

    Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from "@nestjs/core";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 
 async function main() {
     const app = await NestFactory.create(AppModule);
+
+    const config = new DocumentBuilder().setTitle("Blog api").setVersion("1.0").build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup("docs", app, document);
+
     await app.listen(process.env.NODE_PORT);
 }
 
