8e7370ffa2980e045bf55d21c0477ea3bc7b87d1
Marshall Ku <marshall@kakao.com>
2024-02-11 02:00:43 +0900

    Implement user related features

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus } from "@nestjs/common";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
+import { UserService } from "./user.service";
 
-@Controller('user')
-export class UserController {}
+@Controller("user")
+@ApiTags("User API")
+export class UserController {
+    constructor(private userService: UserService) {}
+
+    @HttpCode(HttpStatus.OK)
+    @Get("list")
+    @ApiOperation({ summary: "Get all users" })
+    async getAllUsers() {
+        const user = await this.userService.findAll();
+
+        return user;
+    }
+}
---
+++ src/user/user.schema.ts
@@ -0,0 +1,19 @@
+import { Document, HydratedDocument } from "mongoose";
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { UserRole } from "#constants";
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema({ timestamps: true })
+export class User extends Document {
+    @Prop({ required: true })
+    name: string;
+
+    @Prop({ required: true })
+    password: string;
+
+    @Prop({ default: UserRole.User })
+    role: UserRole;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
