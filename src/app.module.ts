c9ce03e7d5d8fd066874d4a21b7d086207884b9b
Marshall Ku <marshall@kakao.com>
2024-02-10 13:23:35 +0900

    Add config module for parsing .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import * as Joi from "joi";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            envFilePath: `.env`,
+            validationSchema: Joi.object({
+                NODE_PORT: Joi.string().required(),
+            }),
+        }),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
