bdf30a5b93381516949b0ec5d68a6eed6dc2dcb9
Marshall Ku <marshall@kakao.com>
2024-06-21 08:12:50 +0900

    Improve validator to validate url

--- src/comment/comment.validator.ts
+++ src/comment/comment.validator.ts
@@ -1,10 +1,10 @@
 import { z } from "zod";
 
+const koreanRegex = /[ㄱ-ㅎ가-힣]/;
+
 export const commentAddRequestSchema = z.object({
     name: z.string().min(2, "이름을 입력해 주세요.").max(20, "이름은 20자 이내로 입력해 주세요."),
     postSlug: z.string().min(1, "올바르지 않은 요청입니다."),
-    body: z
-        .string()
-        .min(2, "댓글을 입력해 주세요.")
-        .regex(/[ㄱ-ㅎ가-힣]/, "한글을 입력해 주세요."),
+    body: z.string().min(2, "댓글을 입력해 주세요.").regex(koreanRegex, "한글을 입력해 주세요."),
+    url: z.string().url("올바른 URL을 입력해 주세요.").or(z.undefined()).or(z.string().length(0)),
 });
