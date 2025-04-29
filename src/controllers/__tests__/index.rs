e5f49a53aba066baf9645a4cb46c04a435172deb
Marshall Ku <marshall@kakao.com>
2025-04-30 08:58:25 +0900

    Fix way of setting states

--- src/controllers/__tests__/index.rs
+++ src/controllers/__tests__/index.rs
@@ -9,12 +9,14 @@ mod tests {
     };
     use tower::ServiceExt;
 
-    use crate::controllers::index;
+    use crate::{controllers::index, env::state::AppState};
 
     #[tokio::test]
     async fn should_response() {
-        let app: Router = Router::new().route("/", get(index::get));
+        let app: Router<AppState> = Router::new().route("/", get(index::get));
+        let state = AppState::new().await.unwrap();
         let response = app
+            .with_state(state)
             .oneshot(Request::builder().uri("/").body(Body::empty()).unwrap())
             .await
             .unwrap();
