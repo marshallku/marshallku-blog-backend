658732e20fc36096a19cb3d439d70aacb7010950
Marshall Ku <marshall@kakao.com>
2025-05-02 15:59:06 +0900

    Add parse method in Token

--- src/auth/token.rs
+++ src/auth/token.rs
@@ -1,10 +1,17 @@
 use std::collections::BTreeMap;
 
 use hmac::{Hmac, Mac};
-use jwt::SignWithKey;
+use jwt::{Header, SignWithKey, VerifyWithKey};
+use serde::{Deserialize, Serialize};
 use sha2::Sha256;
 
 use crate::models::user::User;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct TokenClaims {
+    pub sub: String,
+    pub username: String,
+}
 
 pub struct Token {}
 
@@ -19,5 +26,19 @@ impl Token {
         let token = claims.sign_with_key(&key)?;
 
         Ok(token)
+    }
+
+    pub fn parse(token: &str, secret_key: &str) -> Result<TokenClaims, Box<dyn std::error::Error>> {
+        let key: Hmac<Sha256> = Hmac::new_from_slice(secret_key.as_bytes())?;
+
+        let token_data: jwt::Token<Header, TokenClaims, _> =
+            token.verify_with_key(&key).map_err(|e| {
+                println!("Error verifying token: {:?}", e);
+                Box::new(e)
+            })?;
+
+        let (_header, claims) = token_data.into();
+
+        Ok(claims)
     }
 }
