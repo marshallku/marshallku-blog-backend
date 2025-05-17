9f68ce97d863d19c24b1d566a587b18afd5c520b
Marshall Ku <marshall@kakao.com>
2025-05-17 23:46:37 +0900

    Add prefix in routes

--- src/controllers/app.rs
+++ src/controllers/app.rs
@@ -5,8 +5,11 @@ use axum::{
 
 use crate::env::state::AppState;
 
+pub const API_VERSION_PREFIX: &str = "/api/v2";
+
 pub fn app() -> Router<AppState> {
     Router::new()
+        // TODO: Remove after migration
         .route("/", get(super::index::get))
         .route("/auth/signin", post(super::auth::signin::post))
         .route("/auth/signup", post(super::auth::signup::post))
@@ -14,4 +17,28 @@ pub fn app() -> Router<AppState> {
         .route("/comment/create", post(super::comments::create::post))
         .route("/comment/list", get(super::comments::list::get))
         .route("/recent", get(super::recent::index::get))
+        .route(
+            &format!("{}/auth/signin", API_VERSION_PREFIX),
+            post(super::auth::signin::post),
+        )
+        .route(
+            &format!("{}/auth/signup", API_VERSION_PREFIX),
+            post(super::auth::signup::post),
+        )
+        .route(
+            &format!("{}/auth/status", API_VERSION_PREFIX),
+            get(super::auth::status::get),
+        )
+        .route(
+            &format!("{}/comment/create", API_VERSION_PREFIX),
+            post(super::comments::create::post),
+        )
+        .route(
+            &format!("{}/comment/list", API_VERSION_PREFIX),
+            get(super::comments::list::get),
+        )
+        .route(
+            &format!("{}/recent", API_VERSION_PREFIX),
+            get(super::recent::index::get),
+        )
 }
