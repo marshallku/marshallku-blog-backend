faa141c1d5c48377a5818ba62525e09a32207070
Marshall Ku <marshall@kakao.com>
2025-06-05 00:04:47 +0900

    Add cors header

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
-use axum::serve;
+use std::env::var;
+
+use axum::{
+    http::{HeaderName, HeaderValue},
+    serve,
+};
 use controllers::app::app;
 use env::state::AppState;
+use reqwest::Method;
 use tokio::{net::TcpListener, signal};
-use tower_http::trace::{DefaultMakeSpan, DefaultOnResponse, TraceLayer};
+use tower_http::{
+    cors::CorsLayer,
+    trace::{DefaultMakeSpan, DefaultOnResponse, TraceLayer},
+};
 use tracing::{info, Level};
 use utils::log::trace_layer_on_request;
 
@@ -23,6 +32,32 @@ async fn main() {
 
     let state = AppState::new().await.unwrap();
     let address = format!("{}:{}", state.host, state.port);
+    let trusted_domains = var("TRUSTED_DOMAINS").unwrap_or_default();
+    let origins = trusted_domains
+        .split(',')
+        .map(|domain| domain.trim())
+        .filter(|domain| !domain.is_empty())
+        .filter_map(|domain| match HeaderValue::from_str(&domain.to_string()) {
+            Ok(value) => Some(value),
+            Err(_) => None,
+        })
+        .collect::<Vec<_>>();
+    let cors_layer = CorsLayer::new()
+        .allow_credentials(true)
+        .allow_headers(vec![
+            HeaderName::from_static("content-type"),
+            HeaderName::from_static("authorization"),
+            HeaderName::from_static("accept"),
+            HeaderName::from_static("origin"),
+        ])
+        .allow_methods(vec![
+            Method::GET,
+            Method::POST,
+            Method::PUT,
+            Method::PATCH,
+            Method::DELETE,
+        ])
+        .allow_origin(origins);
     let app = app()
         .layer(
             TraceLayer::new_for_http()
@@ -30,6 +65,7 @@ async fn main() {
                 .on_response(DefaultOnResponse::new().level(Level::INFO))
                 .on_request(trace_layer_on_request),
         )
+        .layer(cors_layer)
         .with_state(state);
     let listener = TcpListener::bind(address.as_str()).await.unwrap();
 
